bc5cdb1b05a33ce35c2e37e689174c9be2a7deaf
vic3214 <victorhornachos@gmail.com>
2021-03-27 17:39:10 +0100

    Interfaz inicio y primera cara crear personaje

--- app/src/main/java/com/example/trabajocm/MainActivity.java
+++ app/src/main/java/com/example/trabajocm/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.trabajocm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void ejecuta_crea_personaje(View view){
+        Intent i = new Intent(this, Crea_personaje_1.class);
+        startActivity(i);
     }
 }
