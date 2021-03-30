edddeff045e50cfdb51211ab528bc90ee276e475
frankys99 <frankys1999@gmail.com>
2021-03-30 19:41:52 +0200

    Inicio, ventana clase y ventana almacen

---
+++ app/src/main/java/com/example/trabajocm/Clase.java
@@ -0,0 +1,14 @@
+package com.example.trabajocm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Clase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clase);
+    }
+}
