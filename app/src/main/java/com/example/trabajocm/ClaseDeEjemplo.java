629120b1dd818ad52f267e9b5447d2fa0cd44e18
JaredYeeto <jcalcedo.vazquez@gmail.com>
2021-04-04 12:10:50 +0200

    Commit con clase de ejemplo

---
+++ app/src/main/java/com/example/trabajocm/ClaseDeEjemplo.java
@@ -0,0 +1,12 @@
+package com.example.trabajocm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClaseDeEjemplo extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejemplo_layout);
+    }
+}
