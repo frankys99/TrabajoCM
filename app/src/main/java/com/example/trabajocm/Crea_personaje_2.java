225dc50dfdb9d58f2b72f4b02e7b548e4452ca14
JaredYeeto <jcalcedo.vazquez@gmail.com>
2021-04-27 13:52:14 +0200

    Sigo con mi pantalla.

--- app/src/main/java/com/example/trabajocm/Crea_personaje_2.java
+++ app/src/main/java/com/example/trabajocm/Crea_personaje_2.java
@@ -7,6 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ClaseDeEjemplo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ejemplo_layout);
+        setContentView(R.layout.crea_personaje_2);
     }
 }
