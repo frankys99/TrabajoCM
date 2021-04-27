d8383a5a57ef9f02add926b8f507e8ec5d8f3e69
JaredYeeto <jcalcedo.vazquez@gmail.com>
2021-04-27 13:49:42 +0200

    Merge branch 'rama_jose' of C:\Users\jcalc\AndroidStudioProjects\TrabajoCM with conflicts.

--- app/src/main/java/com/example/trabajocm/MisPersonajes.java
+++ app/src/main/java/com/example/trabajocm/MisPersonajes.java
@@ -2,13 +2,27 @@ package com.example.trabajocm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MisPersonajes extends AppCompatActivity {
 
+    Button atras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_personajes);
+
+        atras =findViewById(R.id.atras);
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MisPersonajes.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
