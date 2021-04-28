ba0aecaaf70fc7a80e32c514708805f66b30fb81
JaredYeeto <jcalcedo.vazquez@gmail.com>
2021-04-28 14:00:10 +0200

    Sigo con mi pantalla con ayuda del profesor. Empiezo con la generación de stats aleatorios.

--- app/src/main/java/com/example/trabajocm/Crea_personaje_2.java
+++ app/src/main/java/com/example/trabajocm/Crea_personaje_2.java
@@ -2,28 +2,25 @@ package com.example.trabajocm;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Random;
 import java.util.List;
+
+
 
 public class Crea_personaje_2 extends Activity {
 
+    Random rand = new Random();
     Integer fuerza = 10;
     Integer destreza;
     Integer constitucion;
     Integer sabiduria;
     Integer carisma;
     Integer inteligencia;
+    String cadena;
+
+    private Boolean reroll = Boolean.FALSE;
 
     private EditText edtFuerza;
     private EditText edtDestreza;
@@ -33,14 +30,29 @@ public class Crea_personaje_2 extends Activity {
     private EditText edtInteligencia;
 
     private List<Integer> listaStats;
+    private List<Integer> lm;
 
-
-    public void onclick(View view) {
-        muestraNumeros();
+    private List<Integer> generaNumeros() {
+        for (int i = 0; i < 6; i++) {
+            int n = rand.nextInt(15);
+            n += 3;
+            listaStats.add(n);
+        }
+        return listaStats;
     }
 
-    private void muestraNumeros() {
+    public void onclick(View view) {
+        muestraNumeros(reroll);
+    }
 
+    private void muestraNumeros(Boolean r) {
+        if (r == Boolean.FALSE){
+            lm = generaNumeros();
+            cadena = "Estos son tus stats. Pulsa otra vez Generar para generar nuevos stats";
+
+        } else {
+            lm = generaNumeros();
+        }
     }
 
     protected void onCreate(Bundle savedInstanceState) {
