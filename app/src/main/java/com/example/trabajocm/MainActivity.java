14343f5f2139378946ecb721a729ec3be6e8f4e7
vic3214 <victorhornachos@gmail.com>
2021-05-17 19:10:58 +0200

    Nombres de raza en español

--- app/src/main/java/com/example/trabajocm/MainActivity.java
+++ app/src/main/java/com/example/trabajocm/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.trabajocm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +16,5 @@ public class MainActivity extends AppCompatActivity {
     public void ejecuta_crea_personaje(View view){
         Intent i = new Intent(this, Crea_personaje_1.class);
         startActivity(i);
-    }
-    public void ejecuta_mis_personajes(View view){
-        Intent j = new Intent(this, Mis_personajes.class);
-        startActivity(j);
     }
 }
