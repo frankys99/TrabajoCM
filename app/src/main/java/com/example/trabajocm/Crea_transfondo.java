b91e71776d45ce73c180e6b20d1aa2215e857ab9
JaredYeeto <jcalcedo.vazquez@gmail.com>
2021-05-20 18:11:23 +0200

    Sigo con la pantalla. Trabajando en los spinners

--- app/src/main/java/com/example/trabajocm/Crea_transfondo.java
+++ app/src/main/java/com/example/trabajocm/Crea_transfondo.java
@@ -2,15 +2,19 @@ package com.example.trabajocm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Crea_transfondo extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crea_transfondo);
-
-
     }
 
+    Spinner spinner = (Spinner) findViewById(R.id.spinnerTransfondo);
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.transfondos, android.R.layout.simple_spinner_item);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(adapter);
 }
