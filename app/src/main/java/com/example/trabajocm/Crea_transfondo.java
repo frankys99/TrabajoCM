e6f37f836167e4269e8b7bf5d194c10b58aa3bd0
JaredYeeto <jcalcedo.vazquez@gmail.com>
2021-05-20 17:21:10 +0200

    Empiezo con la pantalla de transfondo

---
+++ app/src/main/java/com/example/trabajocm/Crea_transfondo.java
@@ -0,0 +1,16 @@
+package com.example.trabajocm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Crea_transfondo extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crea_transfondo);
+
+
+    }
+
+}
